f26826b0d68f15b4a357127692e2a00370d58739
Forinarr <shuvalova0723@list.ru>
2026-04-14 16:35:20 +0300

    Add Schedule route to Screen sealed class

--- app/src/main/java/com/shuvalova/student_planner/navigation/Screen.kt
+++ app/src/main/java/com/shuvalova/student_planner/navigation/Screen.kt
@@ -2,15 +2,11 @@ package com.shuvalova.student_planner.navigation
 
 sealed class Screen(val route: String) {
     object Home : Screen("home")
-
     object Details : Screen("details/{subjectId}") {
         fun createRoute(subjectId: String) = "details/$subjectId"
     }
     object Profile : Screen("profile")
     object Settings : Screen("settings")
 
-
-
-
-
+    object Schedule : Screen("schedule")
 }
